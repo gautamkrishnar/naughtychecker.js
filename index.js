9e84cbb9ac3ce72cb0fdb3e1a68e1df8370de9a5
Rohit Jha <r1jha@ucsd.edu>
2017-01-22 00:50:11 -0800

    Cleaned up indentation

--- index.js
+++ index.js
@@ -5,25 +5,26 @@ var blns = JSON.parse(fs.readFileSync("blns.json"));
 var url = "https://raw.githubusercontent.com/minimaxir/big-list-of-naughty-strings/master/blns.json";
 
 module.exports = {
-   strvalidate: function(input) {
-       if (blns.indexOf(input) >= 0) {
-           return true;
-       }
-       return false;
-   },
+  strvalidate: function(input) {
+    if (blns.indexOf(input) >= 0) {
+      return true;
+    }
+    return false;
+  },
 
-   strvalidateonline: function(input) {
-       request({
-           url: url,
-           json: true
-       }, function (error, response, data) {
-             if (!error && response.statusCode === 200) {
-                if (data.indexOf(input) >= 0) {
-                    return true;
-                }
-                return false;
-             }
+  strvalidateonline: function(input) {
+    request({
+        url: url,
+        json: true
+      },
+      function (error, response, data) {
+        if (!error && response.statusCode === 200) {
+          if (data.indexOf(input) >= 0) {
+            return true;
           }
-       );
-   }
+          return false;
+        }
+      }
+    );
+  }
 };
